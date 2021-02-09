9148876f5cb43ed5d8167b89fb29f75020c6929f
Ataur Rahman <rahman.sharif39n@gmail.com>
2021-02-09 23:58:20 +0600

    final update

--- script.js
+++ script.js
@@ -1,84 +1,83 @@
-//search first letter = https://www.themealdb.com/api/json/v1/1/search.php?f=a
-
-function search(){
-
-    let inputValue = document.getElementById("input").value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+const searchBtn = () => {
+    let input = document.getElementById('searchInput').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
+    fetch(url)
     .then(response => response.json())
     .then(data => {
-        displayMeals(data.meals);
+        searchResult(data.meals);
+        console.log(data.meals)
+    })
+    .catch(error => showError(error));
+}
+
+const searchResult = input =>{
+    const resultDiv = document.getElementById("searchResult");
+    resultDiv.innerHTML = '';
+    input.forEach(meal => {
+        const result = document.createElement("div");
+        const resultInfo = `
+            <div  class="col" onclick="showDetails(${meal.idMeal})">
+            <div class="card">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+            </div>
+            </div>
+            </div>
+            ` 
+            result.innerHTML = resultInfo ;
+            resultDiv.appendChild(result);
+    });
+}
+
+const showDetails = id =>{
+    console.log(id);
+    const searchById = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    fetch(searchById)
+    .then(result => result.json())
+    .then(data =>{
+        getData(data.meals[0]);
     })
 }
-    const displayMeals = mealsList => {
-        mealsList.forEach(meals => {
-            const mealsDiv = document.getElementById("allMeal");
-            const mealDiv = document.createElement("div");
-            const mealInfo = `
-                <div class="col" onclick="mealsDetails('${meals.idMeal}')">
-                    <div class="card">
-                        <img src="${meals.strMealThumb}" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">${meals.strMeal}</h5>
-                            <div class = "d-flex justify-content-center">
-                                <button   class="card-text btn btn-dark">Details</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            `
-            mealDiv.innerHTML=mealInfo;
-            mealsDiv.appendChild(mealDiv);
-        })
-    }
 
-    const mealsDetails =idMeal => {
-        const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
-        fetch(url)
-        .then(response => response.json())
-        .then(data =>{
-            console.log(data.meals[0]);
-
-            const mealDetailsDiv = document.createElement('div');
-            const details = `
-        
-                    <div class="col mt-5">
-                        <div class="card text-center">
-                            <img class="rounded mx-auto d-block" src="${data.meals[0].strMealThumb}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title text-center">${data.meals[0].strMeal}</h5>
-                                <p>=>Meal Id : ${data.meals[0].idMeal}</p>
-                                <p> =>${data.meals[0].strIngredient1}</p>
-                                <p>=>${data.meals[0].strIngredient2}</p>
-                                <p>=>${data.meals[0].strIngredient3}</p>
-                                <p>=>${data.meals[0].strIngredient4}</p>
-                                <p>=>${data.meals[0].strIngredient4}</p>
-                                <p>=>${data.meals[0].strIngredient5}</p>
-                                <p>=>${data.meals[0].strIngredient6}</p>
-                                <p>=>${data.meals[0].strIngredient7}</p>
-                                <p>=>${data.meals[0].strIngredient8}</p>
-                                <p>=>${data.meals[0].strIngredient9}</p>
-                                
-                                <div class = "d-flex justify-content-center">
-                                    <button class="card-text btn btn-dark">Back to home</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-        `
-            mealDetailsDiv.innerHTML = details;
-            const mealDetails = document.getElementById('mealDetails');
-            mealDetails.appendChild(mealDetailsDiv);
-            
-        })
-
-    }
-
-
-
+const getData = input =>{
+    console.log(input);
+    const details = document.getElementById("showDetail");
+    details.innerHTML = '';
+    const detailsDiv = document.createElement("div");
+    const detailsDivData = `
     
+    <div id="backToHome"  class="col ">
+    <div class="card">
+      <img src="${input.strMealThumb}" class="card-img-top rounded mx-auto d-block mt-3" alt="...">
+      <div class="card-body">
+        <h3 class="card-title">${input.strMeal}</h3>
+        <h5>StrCategory : ${input.strCategory}</h5>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient1}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient2}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient3}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient4}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient5}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient6}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient7}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient8}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient9}</p>
+        <p><i class="fas fa-arrow-circle-right"></i>  ${input.strIngredient10}</p>
+      </div>
+      <button onclick="backToHome()" class="btn btn-dark">Back to Home</button>
+        </div>
+    </div>
+    `
+    detailsDiv.innerHTML = detailsDivData ;
+    details.appendChild(detailsDiv);
 
+}
 
+const backToHome = () => {
+    document.getElementById("backToHome").style.display = "none";
+}
 
+const showError = () => {
+    document.getElementById("showError").innerText = 'Something is wrong , please try again later .'
 
-
-//  api id = https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meals.id}
+}
